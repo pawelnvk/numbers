2f943cf6e17431e311cdbc481501e74e88d9c7b5
Paweł Nowak <pawel.nowak@netguru.co>
2018-07-15 00:59:54 +0200

    Update styling.

--- stencil.config.js
+++ stencil.config.js
@@ -3,11 +3,6 @@ const sass = require('@stencil/sass');
 
 exports.config = {
   globalStyle: 'src/global/app.css',
-  namespace: 'Numbers',
-  outputTargets: [
-    { type: 'www' },
-    { type: 'docs' }
-  ],
   plugins: [
     sass({
       injectGlobalPaths: ['src/styles/_variables.scss'],
